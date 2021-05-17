935b70131837d96a58f17cf01ef11e6701560b8a
Moulham Hallak <mou990@gmail.com>
2021-05-17 14:34:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const num1 = 31;
 const num2 = 2;
 const multiply = num1 * num2;
 
-let randomInt = Math.floor(Math.random());
+let randomInt = Math.round(Math.random());
 
 const num3 = 16;
 const num4 = 6;
